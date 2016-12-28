642c93b0eba74c9cd19f647a23ff4ff0f23ee719
Luciano Almeida <passos.luciano@outlook.com>
2016-12-28 10:35:15 -0200

    Initial Commit

---
+++ VolumeView/VolumeView.h
@@ -0,0 +1,19 @@
+//
+//  VolumeView.h
+//  VolumeView
+//
+//  Created by Luciano Almeida on 28/12/16.
+//  Copyright © 2016 Luciano Almeida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VolumeView.
+FOUNDATION_EXPORT double VolumeViewVersionNumber;
+
+//! Project version string for VolumeView.
+FOUNDATION_EXPORT const unsigned char VolumeViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VolumeView/PublicHeader.h>
+
+
